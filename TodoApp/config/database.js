bbfda80cf2cec84a3c8a1c5b9ced7a1ccceb614f
Santosh-Lodhi <santoshsinghlodhi@gmail.com>
2017-10-20 14:50:09 +0530

    first commit

---
+++ TodoApp/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    localUrl: 'mongodb://127.0.0.1:27017/todolist_db' //Here 'todolist_db' is db name
+};
---
+++ TodoApp/public/js/core.js
@@ -0,0 +1,19 @@
+var app = angular.module('todoApp', ['todoController', 'todoService']);
+
+
+//-- directive for show confirmation box on Action.  
+
+app.directive('ngConfirmClick', [
+    function(){
+        return {
+            link: function (scope, element, attr) {
+                var msg = attr.ngConfirmClick || "Are you sure?";
+                var clickAction = attr.confirmedClick;
+                element.bind('click',function (event) {
+                    if ( window.confirm(msg) ) {
+                        scope.$eval(clickAction)
+                    }
+                });
+            }
+        };
+}])
